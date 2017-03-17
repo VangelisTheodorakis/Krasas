cd0e252f203337a9e17e53dfdbf64e290bdfdde1
Vangelis Theodorakis <van.theodorakis@gmail.com>
2017-03-17 14:45:52 +0200

    First Chavlis Commit

--- PycharmProjects/Chavlis_Project1/ppca.py
+++ PycharmProjects/Chavlis_Project1/ppca.py
@@ -1,22 +1,24 @@
 import numpy as np
 from numpy.linalg import inv
 from numpy import transpose as tr
-import ipdb
 
 
-class PPCA(object):
+class PPCA():
     def __init__(self, q=2, sigma=1.0):
+        self.w = 0
         self.q = q
         self.prior_sigma = sigma
 
-    def fit(self, y, em=False):
+    def fit(self, y, em=True):
         self.y = y
         self.p = y.shape[0]
         self.n = y.shape[1]
         if em:
             [self.w, self.mu, self.sigma] = self.__fit_em()
         else:
+            """"
             [self.w, self.mu, self.sigma] = self.__fit_ml()
+            """
 
     def transform(self, y=None):
         if y is None:
@@ -24,29 +26,31 @@ class PPCA(object):
         [w, mu, sigma] = [self.w, self.mu, self.sigma]
         m = tr(w).dot(w) + sigma * np.eye(w.shape[1])
         m = inv(m)
-        x = m.dot(tr(w)).dot(y - mu)
+        x = m.dot(tr(w)).dot(y-mu)
         return x
-
+    """
     def fit_transform(self, *args, **kwargs):
         self.fit(*args, **kwargs)
         return self.transform()
 
+    """
+    """
     def transform_infers(self, x=None, noise=False):
         if x is None:
             x = self.transform()
         [w, mu, sigma] = [self.w, self.mu, self.sigma]
         y = w.dot(x) + mu
         if noise:
-            for i in xrange(y.shape[1]):
+            for i in range(y.shape[1]):
                 e = np.random.normal(0, sigma, y.shape[0])
                 y[:, i] += e
         return y
-
+    """
     def __ell(self, w, mu, sigma, norm=True):
         m = inv(tr(w).dot(w) + sigma * np.eye(w.shape[1]))
         mw = m.dot(tr(w))
         ll = 0.0
-        for i in xrange(self.n):
+        for i in range(self.n):
             yi = self.y[:, i][:, np.newaxis]
             yyi = yi - mu
             xi = mw.dot(yyi)
@@ -71,18 +75,20 @@ class PPCA(object):
 
         yy = self.y - mu
         s = self.n**-1 * yy.dot(tr(yy))
-        for i in xrange(maxit):
+        for i in range(maxit):
             m = inv(tr(w).dot(w) + sigma * np.eye(self.q))
             t = inv(sigma * np.eye(self.q) + m.dot(tr(w)).dot(s).dot(w))
             w_new = s.dot(w).dot(t)
             sigma_new = self.p**-1 * np.trace(s - s.dot(w).dot(m).dot(tr(w_new)))
             ll_new = self.__ell(w_new, mu, sigma_new)
-            print "{:3d}  {:.3f}".format(i + 1, ll_new)
+
+            print("{:3d}  {:.3f}" .format(i + 1, ll_new))
+
             w = w_new
             sigma = sigma_new
             ll = ll_new
         return (w, mu, sigma)
-
+""""
     def __fit_ml(self):
         mu = np.mean(self.y, 1)[:, np.newaxis]
         [u, s, v] = np.linalg.svd(self.y - mu)
@@ -98,3 +104,4 @@ class PPCA(object):
         else:
             sigma = 0.0
         return (w, mu, sigma)
+"""
